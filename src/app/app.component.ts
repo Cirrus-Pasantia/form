3552aa2eba3f4a156850f01d116d1367b5a54731
Rodolfo Christian Catunta Uturunco <rodolfo.catunta.uturunco@gmail.com>
2021-05-24 11:13:13 -0400

    Form Microfront

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {dispatcher} from '@cirrusit/flux';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formProject';
+  name = 'GG';
+
+  sendName(name) {
+    console.log('sended name', name);
+    console.log(dispatcher);
+    dispatcher.dispatch({
+      type: 'SEND_NAME',
+      data: name
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'} // Add providers Manually
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
